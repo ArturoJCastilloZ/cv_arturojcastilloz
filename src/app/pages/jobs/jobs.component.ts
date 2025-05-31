4e9804ed54e50a0f74764bdbd664ea41c8bdb24a
ArturoJCastilloZ <castillo.arturo93@hotmail.com>
2025-05-31 15:21:49 -0600

    feat: data in jobs is added

--- src/app/pages/jobs/jobs.component.ts
+++ src/app/pages/jobs/jobs.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterContentChecked, Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { IJobs } from '@Interfaces/global.interface';
+import * as _ from 'underscore';
+import { CommonService } from '../../services/common.service';
+import { DispatcherServices } from '../../services/dispatchers.service';
 
 @Component({
-  selector: 'app-jobs',
-  templateUrl: './jobs.component.html',
-  styleUrls: ['./jobs.component.css']
+    selector: 'app-jobs',
+    templateUrl: './jobs.component.html',
+    styleUrl: './jobs.component.css',
+    standalone: true,
+    imports: [
+        CommonModule,
+    ]
 })
-export class JobsComponent implements OnInit {
+export class JobsComponent implements OnInit, AfterContentChecked {
 
-  constructor() { }
+    jobsData: IJobs[] = [];
+    isMobile = false;
 
-  ngOnInit() {
-  }
+    private readonly _DESTROYREF = inject(DestroyRef);
+    private readonly _DISPATCH = inject(DispatcherServices);
+    private readonly _COMMON = inject(CommonService);
 
+    constructor() {
+    }
+
+    ngOnInit() {
+        this._COMMON.jobsData$.pipe(
+            takeUntilDestroyed(this._DESTROYREF),
+        ).subscribe({
+            next: (data) => {
+                if (data?.length === 0) {
+                    this._DISPATCH.GET_JOBS().pipe(
+                        takeUntilDestroyed(this._DESTROYREF)
+                    ).subscribe({
+                        next: (action) => this.handleGetJobsSuccess(action.response)
+                    });
+                } else this.handleGetJobsSuccess(data)
+            }
+        })
+    }
+
+    handleGetJobsSuccess(action: any) {
+        this.jobsData = _.sortBy(action, function (f) {
+            return f._id
+        }).reverse();
+    }
+
+    ngAfterContentChecked() {
+        this.isMobile = window.innerWidth < 769;
+    }
 }
