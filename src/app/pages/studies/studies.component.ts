632e73bfe5627ee25c4456908c6aed92773792b7
ArturoJCastilloZ <castillo.arturo93@hotmail.com>
2025-05-31 14:41:51 -0600

    feat: add cards in studies component

--- src/app/pages/studies/studies.component.ts
+++ src/app/pages/studies/studies.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { CommonService } from '@Services/common.service';
+import { DispatcherServices } from '@Services/dispatchers.service';
+import { IStudies } from '@Interfaces/global.interface';
 
 @Component({
-  selector: 'app-studies',
-  templateUrl: './studies.component.html',
-  styleUrls: ['./studies.component.css']
+    selector: 'app-studies',
+    templateUrl: './studies.component.html',
+    styleUrls: ['./studies.component.css'],
+    standalone: true,
+    imports: [
+        CommonModule,
+    ]
 })
 export class StudiesComponent implements OnInit {
 
-  constructor() { }
+    studiesData: IStudies[] = [];
 
-  ngOnInit() {
-  }
+    private readonly _DISPATCH = inject(DispatcherServices);
+    private readonly _DESTROYREF = inject(DestroyRef);
+    private readonly _COMMON = inject(CommonService);
+
+    constructor() {
+    }
+
+    ngOnInit() {
+        this._COMMON.studiesData$.pipe(
+            takeUntilDestroyed(this._DESTROYREF),
+        ).subscribe({
+            next: (data) => {
+                if (data?.length === 0) {
+                    this._DISPATCH.GET_STUDIES().pipe(
+                        takeUntilDestroyed(this._DESTROYREF)
+                    ).subscribe({
+                        next: (action) => this.handleGetStudiesSuccess(action.response)
+                    });
+                } else this.handleGetStudiesSuccess(data)
+            }
+        })
+    }
+
+    handleGetStudiesSuccess(action: any) {
+        this.studiesData = action;
+    }
 
 }
