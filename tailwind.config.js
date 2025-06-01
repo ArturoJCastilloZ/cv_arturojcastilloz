a51216d50f0fe9d8d5c4e481849b498d19143d10
ArturoJCastilloZ <castillo.arturo93@hotmail.com>
2025-05-31 23:48:15 -0600

    feat: all components are already added. pending spinner and jwt.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,27 +6,40 @@ module.exports = {
             screens: {
                 'tablet': '640px',
                 // => @media (min-width: 640px) { ... }
-    
+
                 'laptop': '1024px',
                 // => @media (min-width: 1024px) { ... }
-    
+
                 'desktop': '1280px',
                 // => @media (min-width: 1280px) { ... }
                 'sm': '640px',
                 // => @media (min-width: 640px) { ... }
-    
+
                 'md': '768px',
                 // => @media (min-width: 768px) { ... }
-    
+
                 'lg': '1024px',
                 // => @media (min-width: 1024px) { ... }
-    
+
                 'xl': '1280px',
                 // => @media (min-width: 1280px) { ... }
-    
+
                 '2xl': '1536px',
                 // => @media (min-width: 1536px) { ... }
             },
+            "animation": {
+                "fade-in-up": "fade-in-up 0.9s ease-in-out"
+            },
+            "fade-in-up": {
+                "0%": {
+                    "opacity": "0",
+                    "transform": "translateY(20px)"
+                },
+                "100%": {
+                    "opacity": "1",
+                    "transform": "translateY(0)"
+                }
+            }
         },
     },
     plugins: [],
